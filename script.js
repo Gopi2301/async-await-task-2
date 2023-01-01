28958b8938572854956279a44f7798246e42260c
Gopi2301 <118594301+Gopi2301@users.noreply.github.com>
2023-01-01 23:33:27 +0530

    anime api
    async await

---
+++ script.js
@@ -0,0 +1,23 @@
+var url = ("https://animechan.vercel.app/api/quotes/anime?title=naruto")
+
+async function reqFetch() {
+    var urlFetch = await fetch(url)
+    var urlData = await urlFetch.json()
+    urlData.map((ele) => {
+        var parent = document.querySelector('.imageBox')
+        var cardData = document.createElement('div')
+        cardData.innerHTML = `
+        <div class='col'>
+        <div class='card h-100'>
+        <div class='card-body'>
+        <h2 class='card-title text-center'>${ele.anime}</h2>
+        <h5 class='card-text character'>${ele.character}</h5>
+        <p class='card-text qoute'>${ele.quote}</p>
+
+
+     `
+        parent.append(cardData)
+        console.log(ele)
+    })
+}
+reqFetch();
